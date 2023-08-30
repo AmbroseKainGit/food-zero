1f3d226a5033328e2fb118360d3b6779c491d3bb
Joshua030 <Joseluy123>
2023-08-30 21:27:45 +0200

    Modify Home add images and title

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,28 @@
-'use client'
+"use client";
+
+import Image from "next/image";
 
 export default function Landing() {
-
   return (
-   <div className="main-header-container">
-    <header className="header-container">
-
-    </header>
-   </div>
+    <div className="main-header-container">
+      <header className="header-container">
+        <div className="hero-header-title">
+          <h2>Healthy Eating is important part of lifestyle</h2>
+          <p>Discovering Nutritional Delights: A Path to Wellness</p>
+        </div>
+        <Image
+          src="/static/imagen-plato-carne.webp"
+          width={792}
+          height={931}
+          alt="Plate with a Piece of Meat"
+        />
+        <Image
+          src="/static/spices.webp"
+          width={260}
+          height={240}
+          alt="Plate of Spices"
+        />
+      </header>
+    </div>
   );
 }
