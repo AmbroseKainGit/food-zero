de091194bc857c0fae3418aa5baf4581b4651221
Cristian Ceballos <cmceballos88@gmail.com>
2023-09-03 01:45:56 +0200

    Fix scroll when showing cover page

--- app/template.tsx
+++ app/template.tsx
@@ -2,9 +2,19 @@
 import Cover from "@/components/General/Cover";
 import { useAppSelector } from "@/hooks/redux";
 import { selectCover } from "@/lib/redux";
+import { useEffect } from "react";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   const showCover = useAppSelector(selectCover);
+  
+  useEffect(() => {
+    document.body.style.overflowY = showCover ? 'hidden' : 'unset';
+    document.body.style.height = showCover ? '100vh' : 'auto';
+    return () => {
+      document.body.style.overflowY = 'unset'; 
+      document.body.style.height = 'unset'; 
+    };
+  }, [showCover]);
   return (
     <>
       <Cover />
