8655f3ff64d6f00c198a82bebeda036c98531677
Joshua030 <Joseluy123>
2023-09-05 19:56:45 +0200

    Feat,Home Section three

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
 "use client";
+import { CallToAction } from "@/components/Home/CallToAction";
+import { FirstAd } from "@/components/Home/FirstAd";
 import { Header } from "@/components/Home/Header";
 import { useAppSelector } from "@/hooks/redux";
 import { selectCover } from "@/lib/redux";
 export default function Landing() {
   return (
-    <div className="main-header-container">
-      <Header />
-    </div>
+    <>
+      <div className="main-header-container">
+        <Header />
+      </div>
+      <div className="call-action-main">
+        <CallToAction />
+      </div>
+      <div className="advertising-first-container">
+        <FirstAd />
+      </div>
+    </>
   );
 }
