30dc675d6001f0f124cb5f5279944936686f9f2a
Cristian Ceballos <cmceballos88@gmail.com>
2023-09-12 01:24:43 +0200

    Remove comment

--- lib/graphql/provider.tsx
+++ lib/graphql/provider.tsx
@@ -27,7 +27,7 @@ const authLink = setContext((_, { headers }) => {
 });
 
 const client = new ApolloClient({
-    link: authLink.concat(httpLink), // Chain it with the httpLink
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
   });
 
