f61d171a2183a3a7f930f49b0beac6e7fd64a28d
Cristian Ceballos <cmceballos88@gmail.com>
2023-09-12 01:24:18 +0200

    Update provider so it sends token when needed

--- lib/graphql/provider.tsx
+++ lib/graphql/provider.tsx
@@ -1,28 +1,42 @@
-'use client';
+"use client";
 
 import {
- ApolloClient,
- ApolloProvider,
- InMemoryCache,
-} from '@apollo/client';
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-const client = new ApolloClient({
- uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT || 'http://localhost:4000/graphql',
- cache: new InMemoryCache(),
+const httpLink = createHttpLink({
+  uri:
+    process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT || "http://localhost:4000/graphql"
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    return { headers };
+  }
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${token}`
+    }
+  };
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink), // Chain it with the httpLink
+    cache: new InMemoryCache(),
+  });
+
 interface IGraphQlProviderProps {
- children: React.ReactNode;
+  children: React.ReactNode;
 }
 
-const GraphQlProvider: React.FC<IGraphQlProviderProps> = ({
- children,
-}) => {
- return (
-  <ApolloProvider client={client}>
-   {children}
-  </ApolloProvider>
- );
+const GraphQlProvider: React.FC<IGraphQlProviderProps> = ({ children }) => {
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
 };
 
 export default GraphQlProvider;
