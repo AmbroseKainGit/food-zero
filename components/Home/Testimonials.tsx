cdcad88c7b0b8d019f906ac71a5765639b96c741
Cristian Ceballos <cmceballos88@gmail.com>
2023-09-25 17:17:57 +0200

    Render only when ref is done

--- components/Home/Testimonials.tsx
+++ components/Home/Testimonials.tsx
@@ -1,27 +1,26 @@
-'use client'
-import React, { useEffect, useState, useRef } from "react";
+"use client";
+import React, { useEffect, useState, useRef, useLayoutEffect } from "react";
 import data from "@/data/fakeDataComments.json";
 import { TestimonialCard } from "./components/TestimonialCard";
 import { Carousel } from "@trendyol-js/react-carousel";
 
 export const Testimonials = () => {
-  const [carouselShow, setCarouselShow] = useState(2.5)
-  const [carouselSlide, setCarouselSlide] = useState(2)
+  const [carouselShow, setCarouselShow] = useState(0);
+  const [carouselSlide, setCarouselSlide] = useState(0);
   const [width, setWidth] = useState<number>(0);
   const ref = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const resizeObserver = new ResizeObserver((entries) => {
       const newWidth = entries[0]?.contentRect.width;
       setWidth(newWidth);
-      if(newWidth <= 1010) {
+      if (newWidth <= 1010) {
         setCarouselShow(1.5);
         setCarouselSlide(1);
       } else {
         setCarouselShow(2.5);
         setCarouselSlide(2);
       }
-      
     });
 
     resizeObserver.observe(ref.current!);
@@ -31,29 +30,30 @@ export const Testimonials = () => {
     };
   }, [width]);
 
-
-
   return (
     <div className="testimonials-inner-container" ref={ref}>
       <h3>Rate & Relate: Share Your Restaurant Experience!</h3>
-      <Carousel
-        show={carouselShow}
-        slide={carouselSlide}
-        swiping={true}
-        responsive={true}
-        className="testiomnials-container-home"
-      >
-        {data.map(({ name, id, stars, comment }) => {
-          return (
-            <TestimonialCard
-              key={id}
-              name={name}
-              comment={comment}
-              stars={stars}
-            />
-          );
-        })}
-      </Carousel>
+      {ref.current && (
+        <Carousel
+          show={carouselShow}
+          slide={carouselSlide}
+          swiping={true}
+          responsive={true}
+          dynamic={true}
+          className="testiomnials-container-home"
+        >
+          {data.map(({ name, id, stars, comment }) => {
+            return (
+              <TestimonialCard
+                key={id}
+                name={name}
+                comment={comment}
+                stars={stars}
+              />
+            );
+          })}
+        </Carousel>
+      )}
     </div>
   );
 };
