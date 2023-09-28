a025dfc37b4e1991b5d4d2bf6148f511966fff05
Cristian Ceballos <cmceballos88@gmail.com>
2023-09-28 17:53:20 +0200

    Fix blog

--- components/Blog/MainSectionBlog.tsx
+++ components/Blog/MainSectionBlog.tsx
@@ -3,7 +3,7 @@ import { BlogCard } from "../Home/components/BlogCard";
 
 export const MainSectionBlog = () => {
     return <div className="blog-container-page">
-    {data.map(({image,title,content,id,slug})=>{
+    {data.slice(2).map(({image,title,content,id,slug})=>{
         return(
           <BlogCard key={id} image={image} title={title} content={content} slug={slug}/>
         )
